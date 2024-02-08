f4ee7ca4f048029a91c509e836f6ae6f6f06e6b6
stevan06v <stevanvlajic5@gmail.com>
2024-02-08 13:56:54 +0100

    initial commit

---
+++ propromo/propromoApp.swift
@@ -0,0 +1,17 @@
+//
+//  propromoApp.swift
+//  propromo
+//
+//  Created by Stevan Vlajic on 08.02.24.
+//
+
+import SwiftUI
+
+@main
+struct propromoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
