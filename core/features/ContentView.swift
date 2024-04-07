b0dd13d850be68b2be0eedacccc36a825686ced1
stevan06v <stevanvlajic5@gmail.com>
2024-04-07 20:37:01 +0200

    feat: quickfix ig -still searchng for optimal solution

--- core/features/ContentView.swift
+++ core/features/ContentView.swift
@@ -1,38 +1,49 @@
 import SwiftUI
+import Combine
+
 
 struct ContentView: View {
-    
     @EnvironmentObject var router: Router
+    @AppStorage("AUTH_KEY") var authenticated: Bool = false
+
+    
     var body: some View {
-        TabView {
-            VStack {
-                RegistrationView(router: router)
-            }.tabItem() {
-                Label("Home", systemImage: "house")
-            }.padding()
-            
-            VStack {
-                Text("Projects")
-            }.tabItem() {
-                Label("Projects", systemImage: "square.stack.3d.up")
-            }.badge(69)
-            
-            VStack {
-                Text("Chat")
-            }.tabItem() {
-                Label("Chat", systemImage: "text.bubble.fill")
+
+        if !authenticated {
+            RegistrationView(router: router)
+        } else {
+            TabView {
+                HomeView()
+                VStack {
+                }.tabItem() {
+                    Label("Home", systemImage: "house")
+                }.padding()
+                
+                VStack {
+                    Text("Projects")
+                }.tabItem() {
+                    Label("Projects", systemImage: "square.stack.3d.up")
+                }.badge(69)
+                
+                VStack {
+                    Text("Chat")
+                }.tabItem() {
+                    Label("Chat", systemImage: "text.bubble.fill")
+                }
+                VStack {
+                    Text("Settings")
+                }.tabItem() {
+                    Label("Settings", systemImage: "gear")
+                }
+                
             }
-            VStack {
-                Text("Settings")
-            }.tabItem() {
-                Label("Settings", systemImage: "gear")
-            }
-            
         }
+        
     }
 }
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        let router = Router()
+                ContentView().environmentObject(router)
     }
 }
