233b64225b900fe0276baf24b9e1282de1261a39
stevan06v <stevanvlajic5@gmail.com>
2024-04-11 14:19:26 +0200

    feat: added magical fix :)

--- core/features/ContentView.swift
+++ core/features/ContentView.swift
@@ -6,42 +6,36 @@ struct ContentView: View {
     @EnvironmentObject var router: Router
     @AppStorage("AUTH_KEY") var authenticated: Bool = false
     
-    
     var body: some View {
-        
-    // if !authenticated {
-        //RegistrationView(router: router)
-            LogInView(router: router)
-    //   } else {
-            /*
-            TabView {
-                
-                
-                VStack {
-                }.tabItem() {
-                    Label("Home", systemImage: "house")
-                }.padding()
-                
-                VStack {
-                    Text("Projects")
-                }.tabItem() {
-                    Label("Projects", systemImage: "square.stack.3d.up")
-                }.badge(69)
-                
-                VStack {
-                    Text("Chat")
-                }.tabItem() {
-                    Label("Chat", systemImage: "text.bubble.fill")
+        ZStack{
+            if !authenticated {
+                RegistrationView(router: router)
+            } else {
+                TabView {
+                    VStack {
+                        HomeView()
+                    }.tabItem() {
+                        Label("Home", systemImage: "house")
+                    }.padding()
+                    VStack {
+                        Text("Projects")
+                    }.tabItem() {
+                        Label("Projects", systemImage: "square.stack.3d.up")
+                    }.badge(69)
+                    
+                    VStack {
+                        Text("Chat")
+                    }.tabItem() {
+                        Label("Chat", systemImage: "text.bubble.fill")
+                    }
+                    VStack {
+                        Text("Settings")
+                    }.tabItem() {
+                        Label("Settings", systemImage: "gear")
+                    }
                 }
-                VStack {
-                    Text("Settings")
-                }.tabItem() {
-                    Label("Settings", systemImage: "gear")
-                }
-                
             }
-        }*/
-        
+        }
     }
 }
 struct ContentView_Previews: PreviewProvider {
