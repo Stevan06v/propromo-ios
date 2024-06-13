d04003e5e14d3cf692241e194c900cbd338a150a
jonasfroeller <publik.froeller.jonas@gmx.at>
2024-06-13 08:58:25 +0200

    feat(App): force portrait mode (vertical view)

---
+++ core/features/AppDelegate.swift
@@ -0,0 +1,11 @@
+import Foundation
+import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+        
+    static var orientationLock = UIInterfaceOrientationMask.all
+
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return AppDelegate.orientationLock
+    }
+}
--- core/features/propromoApp.swift
+++ core/features/propromoApp.swift
@@ -3,9 +3,18 @@ import SwiftUI
 
 @main
 struct propromoApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+            .onAppear {
+                UIDevice.current.setValue(UIInterfaceOrientation.portrait.rawValue, forKey: "orientation")
+                AppDelegate.orientationLock = .portrait
+            }
+            //.onDisappear {
+            //    AppDelegate.orientationLock = .all
+            //}
         }
     }
 }
